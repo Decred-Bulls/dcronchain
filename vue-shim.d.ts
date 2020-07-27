07441d52f7fe1ccb12bf79177ebeba6f68f9a3f0
Pavel Svitek <pavel.svitek@maklando.ch>
2020-07-27 20:50:01 +0200

    vue-shim.d.ts - add svg module definitions

--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -2,3 +2,17 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+// https://vue-svg-loader.js.org/faq.html#how-to-use-this-loader-with-typescript
+declare module '*.svg' {
+  import Vue, { VueConstructor } from 'vue'
+  const content: VueConstructor<Vue>
+  export default content
+}
+
+// https://vue-svg-loader.js.org/faq.html#how-to-use-this-loader-with-typescript
+declare module '*.svg?inline' {
+  import Vue, { VueConstructor } from 'vue'
+  const content: VueConstructor<Vue>
+  export default content
+}
