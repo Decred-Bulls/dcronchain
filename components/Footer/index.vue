4794e01a418eb9eece1b7b64115ff23d11e2926b
Pavel Svitek <pavel.svitek@maklando.ch>
2020-09-06 19:55:03 +0200

    components/Footer - redesign + mobile opts

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="c-footer mt-50">
     <div class="c-footer__content">
-      <div>
+      <div class="c-footer__row">
+        <div class="c-footer__links2">
+          <a href="#">Decred.org</a>
+          <a href="#">Docs</a>
+          <a href="#">Wallet</a>
+          <a href="#">Community</a>
+          <a href="#">Become a contributor</a>
+        </div>
+
         <div class="c-footer__social-links">
           <IconDiscord />
           <IconMatrix />
@@ -10,7 +18,7 @@
         </div>
       </div>
 
-      <div class="c-footer__links ml-40">
+      <!-- <div class="c-footer__links ml-40">
         <label>Popular Entries</label>
         <a href="#">Link 1</a>
         <a href="#">Link 2</a>
@@ -25,15 +33,17 @@
         <a href="#">Link 3</a>
         <a href="#">Link 4</a>
         <a href="#">Link 5</a>
-      </div>
-    </div>
-    <div class="m--flex column mt-20">
-      <Logo style="width: 230px; height: 30px;" />
+      </div> -->
+      <div class="c-footer__row mt-50">
+        <div>
+          <Logo style="width: 230px; height: 30px;" />
+        </div>
 
-      <p class="c-footer__copyright mt-1">
-        © 2020 Decred On Chain. Built by Decred community for people of the
-        Internet.
-      </p>
+        <p class="c-footer__copyright">
+          © 2020 Decred On Chain. Built by Decred community for people of the
+          Internet.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -66,10 +76,60 @@ export default Vue.extend({
   padding: 40px 0;
 
   &__content {
+    width: 100%;
     max-width: 1200px;
     margin: 0 auto;
     display: flex;
     justify-content: center;
+    flex-direction: column;
+
+    @include media('<=sm') {
+      max-width: 100%;
+      padding: 0 30px;
+    }
+  }
+
+  &__row {
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+    @include media('<=sm') {
+      flex-direction: column;
+    }
+  }
+
+  &__links2 {
+    /* font-family: Source Sans Pro; */
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 25px;
+    color: $color-gray-600;
+
+    @include media('<=sm') {
+      display: flex;
+      flex-direction: column;
+      margin-left: 0;
+
+      & > * {
+        margin-top: 8px;
+      }
+    }
+
+    & > * {
+      &:not(:last-child) {
+        margin-right: 20px;
+      }
+    }
+
+    a,
+    a:active,
+    a:hover,
+    a:visited {
+      color: $color-gray-600;
+      text-decoration: none;
+    }
   }
 
   &__links {
@@ -104,6 +164,18 @@ export default Vue.extend({
 
   &__social-links {
     display: flex;
+    margin-left: auto;
+
+    @include media('<=sm') {
+      margin-left: 0;
+      margin-top: 50px;
+
+      & > * {
+        &:not(:last-child) {
+          margin-right: 30px;
+        }
+      }
+    }
 
     & > svg {
       width: 22px;
@@ -118,6 +190,14 @@ export default Vue.extend({
     font-size: 16px;
     line-height: 20px;
     color: $color-gray-600;
+    margin-left: auto;
+    margin-top: 4px;
+
+    @include media('<=sm') {
+      margin-left: 0;
+      margin-top: 12px;
+      text-align: center;
+    }
   }
 }
 </style>
