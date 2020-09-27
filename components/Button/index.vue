172ab81bed20ad20504535b2c0e183a3d190a63f
Pavel Svitek <pavel.svitek@maklando.ch>
2020-09-27 20:27:34 +0200

    Button - add props: transparent, size

--- components/Button/index.vue
+++ components/Button/index.vue
@@ -3,7 +3,12 @@
     v-bind="$attrs"
     v-on="$listeners"
     class="c-button"
-    :class="{ 'is-inverted': inverted === true }"
+    :class="{
+      'is--inverted': inverted === true,
+      'is--size-normal': size === 'normal',
+      'is--size-big': size === 'big',
+      'is--transparent': transparent === true,
+    }"
   >
     <span v-if="!!prependIcon" class="c-button__icon mr-3">
       <IconArrowLeft v-if="prependIcon === 'arrow-left'" />
@@ -25,6 +30,7 @@ import IconArrowLeft from './arrow-left.svg?inline'
 import IconArrowRight from './arrow-right.svg?inline'
 
 type IconType = 'arrow-left' | 'arrow-right'
+type SizeType = 'normal' | 'big'
 
 export default Vue.extend({
   components: {
@@ -48,6 +54,16 @@ export default Vue.extend({
       required: false,
       default: () => false,
     } as PropOptions<Boolean>,
+    size: {
+      type: String,
+      required: false,
+      default: () => 'normal',
+    } as PropOptions<SizeType>,
+    transparent: {
+      type: Boolean,
+      required: false,
+      default: () => false,
+    } as PropOptions<Boolean>,
   },
 })
 </script>
@@ -67,9 +83,17 @@ export default Vue.extend({
   border-radius: 4px;
   padding: 10px 20px;
 
-  &.is-inverted {
+  &.is--size-big {
+    padding: 15px 30px;
+  }
+
+  &.is--inverted {
     color: #fff;
     background-color: $color-blue-100;
+  }
+
+  &.is--transparent {
+    background-color: transparent;
   }
 
   svg {
